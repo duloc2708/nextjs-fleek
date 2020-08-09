0d5f02dc27c5bc105f546f7ac5d2d583dd7ba93c
Thomas Greco <tgreco1610@gmail.com>
2020-08-09 03:51:13 -0400

    added export command

---
+++ .next/static/MleFVosXC3R8lA_0Op8CY/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST = {"/":["static\u002Fchunks\u002Fpages\u002Findex-d079e5e52b9de015f1ce.js"],"/_error":["static\u002Fchunks\u002Fpages\u002F_error-a98d98d178927e175cb7.js"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
