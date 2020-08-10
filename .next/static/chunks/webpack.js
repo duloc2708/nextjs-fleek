d0ead388b238c0914bc7954d9aa35af980b7b1ff
Thomas Greco <tgreco1610@gmail.com>
2020-08-10 02:40:15 -0400

    added updates

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "0286cc5c170d3f2a23d3";
+/******/ 	var hotCurrentHash = "0cbffe5bf71506e156e8";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
