69f2d76684637773fde19b880dfdcd8b9af37a56
Thomas Greco <tgreco1610@gmail.com>
2020-08-10 02:40:35 -0400

    added ability to query data from chainlink oracles

---
+++ next.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  env: {
+    infuraMainnet: "https://mainnet.infura.io/v3/",
+    infuraID: "cc37f552c1354792b23c88d090c4a02f",
+    INFURA_ROPSTEN: "https://ropsten.infura.io/v3/",
+    INFURA_RINKBY: "https://rinkeby.infura.io/v3/",
+    INFURA_MAINNET: "https://mainnet.infura.io/v3/",
+  },
+};
